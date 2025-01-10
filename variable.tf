b955571c6a2851755c8a7930babc0ed59b696b41
sisindrikilari <142840325+sisindrikilari@users.noreply.github.com>
2025-01-10 12:20:51 -0500

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "ami_id" {
+type = string
+default = "ami-03eb6185d756497f8"
+}
+
+variable "itype" {
+type = string
+default = "t2.micro"
+}
+
+variable "kn" {
+type = string
+default = "project-keypair"
+}
+
+variable "iname" {
+type = string
+default = "Swiggy"
+}
+
